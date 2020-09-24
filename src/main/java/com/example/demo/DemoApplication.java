eca1f6fad9376c61c87952a081ccbad087b4f429
Pengjie Zhou <zhoupj1215@hotmail.com>
2020-09-24 10:18:58 -0700

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication extends SpringBootServletInitializer{
+    // 用来测试访问
+    @RequestMapping("/")
+    public String home() {
+        return "hello 朋友";
+    }
+
+    @RequestMapping("/hello")
+    public String home2() {
+        return "hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    // 继承SpringBootServletInitializer 实现configure 方便打war 外部服务器部署。
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+}
